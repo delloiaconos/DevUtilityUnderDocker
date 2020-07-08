8e7dbc7c88d0f57e1d677cc3f7c8e255c2d3699a
Salvatore Dello Iacono <delloiaconos@gmail.com>
2020-07-08 11:10:12 +0200

    modified:   SubversionEdge/Dockerfile 	modified:   SubversionEdge/files/etc/supervisor.d/csvnautostarter.conf 	modified:   SubversionEdge/files/opt/csvnautostarter/csvnautostarter.sh 	modified:   docker-compose.yml

--- SubversionEdge/files/opt/csvnautostarter/csvnautostarter.sh
+++ SubversionEdge/files/opt/csvnautostarter/csvnautostarter.sh
@@ -2,18 +2,20 @@
 
 CSVN_BIN=/opt/csvn/bin/
 
-echo "HELLO WORLD\n\n" >> /opt/csvn/myscript	
+while true
+do
+	$CSVN_BIN/csvn status | grep "is not running" &> /dev/null 
 
-$CSVN_BIN/csvn status | grep "is not running" &> /dev/null 
+	if [ $? == 0 ]; then
+		echo "CSVN Not Running!"
+		$CSVN_BIN/csvn start
+	fi
 
-if [ $? == 0 ]; then
-	echo "CSVN Not Running!"
-	$CSVN_BIN/csvn start
-fi
+	$CSVN_BIN/csvn-httpd status | grep "Not Running" &> /dev/null
 
-$CSVN_BIN/csvn-httpd status | grep "Not Running" &> /dev/null
-
-if [ $? == 0 ]; then
-	echo "CSVN-HTTPD Not Running!"
-	$CSVN_BIN/csvn-httpd start
-fi
+	if [ $? == 0 ]; then
+		echo "CSVN-HTTPD Not Running!"
+		$CSVN_BIN/csvn-httpd start
+	fi
+	sleep 300;
+done;
