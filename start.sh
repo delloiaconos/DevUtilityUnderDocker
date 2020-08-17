88d05cccb731a425faf28ae282c12a24fdf81d68
Salvatore Dello Iacono <delloiaconos@gmail.com>
2020-08-17 20:25:57 +0000

    -- ADDED: bugzilla-service + mysql-service -- docker-compose contains all the containers

--- start.sh
+++ start.sh
@@ -8,21 +8,27 @@ LDAP_ORGANISATION="Example"
 LDAP_DOMAIN="example.org"
 LDAP_ADMIN_PASSWORD="password123"
 
+
+## COMMON CONFIGS
+BASE_PATH="/opt/devutility"
+
+
 docker rm ldap-service -f
 docker rm phpldapadmin-service -f
 docker rm subversion-service -f
 docker rm ldappasswd-service -f
 
+
 ## LDAP SERVICE
-mkdir -p /opt/devutility/ldap-db
-mkdir -p /opt/devutility/ldap-conf
+mkdir -p $BASE_PATH/ldap-db
+mkdir -p $BASE_PATH/ldap-conf
 
 docker run \
    --name ldap-service \
    --hostname ldap-service \
    --restart always \
-   --volume /opt/devutility/ldap-db:/var/lib/ldap \
-   --volume /opt/devutility/ldap-conf:/etc/ldap/slapd.d \
+   --volume $BASE_PATH/ldap-db:/var/lib/ldap \
+   --volume $BASE_PATH/ldap-conf:/etc/ldap/slapd.d \
    -p 389:389 -p 636:636 \
    --env LDAP_ORGANISATION="${LDAP_ORGANISATION}" \
    --env LDAP_DOMAIN="${LDAP_DOMAIN}" \
@@ -48,7 +54,7 @@ echo "Password: admin"
 
 
 ## SUBVERSIONEDGE SERVICE
-mkdir -p /opt/devutility
+mkdir -p $BASE_PATH/svndata
 
 docker build -f SubversionEdge/Dockerfile -t subversionedge ./SubversionEdge
 docker run \
@@ -56,20 +62,52 @@ docker run \
    --hostname subversion-service \
    --restart always \
    -p 4434:4434 -p 3343:3343 -p 18080:18080 \
-   --volume /opt/devutility/svndata:/opt/csvn/data \
+   --volume $BASE_PATH/svndata:/opt/csvn/data \
    --detach subversionedge
 
-## LDAPPASSWDWEBUI SERVICE
-docker build -f LDAPPasswdWebui/Dockerfile -t ldappasswdwebui ./LDAPPasswdWebui
+## SELFSERVICEPASSWORD SERVICE
+docker build -f SelfServicePassword/Dockerfile -t ldappasswd ./SelfServicePassword
 
-mkdir -p /opt/devutility/ldappasswd
-cp ./LDAPPasswdWebui/settings.ini /opt/devutility/ldappasswd
+mkdir -p $BASE_PATH/ldappasswd
+cp ./SelfServicePassword/conf/* $BASE_PATH/ldappasswd
 
 docker run \
    --name ldappasswd-service \
    --hostname ldappasswd-service \
    --restart always \
-   -p 9090:8080 \
-   --volume /opt/devutility/ldappasswd:/conf \
-   --env CONF_FILE="/conf/settings.ini" \
-   --detach ldappasswdwebui
+   -p 9090:80 \
+   --volume $BASE_PATH/ldappasswd:/var/www/conf \
+   --detach ldappasswd
+
+## NETWORK
+docker network create \
+   --driver=bridge \
+   --subnet=172.28.0.0/24 \
+   --ip-range=172.28.0.0/24 \
+   --gateway=172.28.0.254 \
+  br0
+
+## MYSQL SERVICE
+docker run \
+   --name mysql-service \
+   --hostname mysql-service \
+   --restart always \
+   --ip 172.28.0.100 br0 \
+   -e MYSQL_ROOT_PASSWORD="MySqlRootPassword" \
+   -e MYSQL_USER="mysqluser" \
+   -e MYSQL_PASSWORD="userpassword" \
+   --detach mysql:8.0
+
+
+## BUGZILLA SERVICE
+docker run \
+   --name bugzilla-service \
+   --hostname bugzilla-service \
+   --restart always \
+   --ip 172.28.0.101 br0 \
+   -p 8000:80 \
+   --volume $BASE_DIR/bugzilla:/etc/msmtprc:ro \
+   -e MYSQL_DB="172.28.0.100" \
+   -e MYSQL_USER="mysqluser" \
+   -e MYSQL_PWD="userpassword" \
+   --detach achild/bugzilla
